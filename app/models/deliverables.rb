f5757d14781802e1df5580c53195127767e065f2
Rahul Baxi <rahul.baxi@sv.cmu.edu>
2010-11-04 10:37:39 -0700

    Configured deliverables.rb to save files on amazon s3

--- app/models/deliverables.rb
+++ app/models/deliverables.rb
@@ -3,7 +3,13 @@ class Deliverables < ActiveRecord::Base
   belongs_to :course
   belongs_to :team
 
-  has_attached_file :zip, :url => "/:attachment/:id_:style.:extension", :path => ":rails_root/public/:attachment/:id_:style.:extension"
+#   :url => "/:attachment/:id_:style.:extension",
+#    :path => ":rails_root/public/:attachment/:id_:style.:extension",
+   has_attached_file :zip,
+     :storage => :s3,
+    :s3_credentials => "#{RAILS_ROOT}/config/amazon_s3.yml",
+    :path => ":attachment/:id/#{:zip_file_name}.:extension",
+    :bucket => 'cmusv-rails-mfse-development'
 
   #validates_presence_of :submission_date, :person
 
